2fb40f9e8287045b64eee8cdc939347d9b807b21
Lucas Roberts <rlucas7@vt.edu>
2023-03-09 23:06:56 -0500

    Update scipy/special/cephes.h
    Co-authored-by: Albert Steppi <albert.steppi@gmail.com>

--- scipy/special/cephes.h
+++ scipy/special/cephes.h
@@ -109,7 +109,7 @@ extern double round(double x);
 extern int shichi(double x, double *si, double *ci);
 extern int sici(double x, double *si, double *ci);
 
-extern int stirling2(int n, int k);
+long int stirling2(int n, int k);
 
 extern double radian(double d, double m, double s);
 extern double sindg(double x);
