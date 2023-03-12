aeecb266cdd38a465a413064e0b7660f0b0c8525
steppi <albert_steppi@hms.harvard.edu>
2023-03-12 12:27:59 -0400

    Fix bug, returning signed expecting unsigned

--- scipy/special/cephes/stirling.c
+++ scipy/special/cephes/stirling.c
@@ -32,11 +32,11 @@ static uint64_t stirling2(long n, long k){
     /* Don't need to check for edge cases. This function is only meant to be
      * called from stirling2_approx which checks for these cases.
      */
-    int arraySize = k <= n - k + 1 ? k : n - k + 1;
+    unsigned int arraySize = k <= n - k + 1 ? k : n - k + 1;
     uint64_t *curr = malloc(arraySize * sizeof(uint64_t));
     if (!curr) {
         sf_error("stirling2", SF_ERROR_NO_RESULT, "failed to allocate memory");
-        return -2;
+        return 0;
     }
     /* Computes from the bottom up using the recurrence relation
      * stirling2(n, k) = k * stirling2(n, k - 1) + stirling2(n - 1, k - 1)
@@ -63,44 +63,44 @@ static uint64_t stirling2(long n, long k){
      * to bottom and then left to right. See
      * https://github.com/scipy/scipy/pull/18103#discussion_r1130036365
      */
-    for (int i = 0; i < arraySize; i++){
+    for (unsigned int i = 0; i < arraySize; i++){
         curr[i] = 1;
     }
     uint64_t tmp;
     if (k <= n - k + 1) {
-        for (int i = 1; i < n - k + 1; i++){
-            for (int j = 1; j < k; j++){
+        for (unsigned int i = 1; i < n - k + 1; i++){
+            for (unsigned int j = 1; j < k; j++){
 	      if (curr[j] > UINT64_MAX / (j + 1)) {
 		free(curr);
 		sf_error("stirling2", SF_ERROR_NO_RESULT,
 		         "integer overflow has occured");
-		return -1;
+		return 0;
 	      }
 	      tmp = curr[j] * (j + 1);
 	      if (tmp > UINT64_MAX - curr[j - 1]) {
 		free(curr);
 		sf_error("stirling2", SF_ERROR_NO_RESULT,
 		         "integer overflow has occured");
-		return -1;
+		return 0;
 	      }
 	      curr[j] = tmp + curr[j - 1];
             }
         }
     } else {
-        for (int i = 1; i < k; i++){
-            for (int j = 1; j < n - k + 1; j++){
+        for (unsigned int i = 1; i < k; i++){
+            for (unsigned int j = 1; j < n - k + 1; j++){
 	      if (curr[j - 1] > UINT64_MAX / (i + 1)){
 		free(curr);
 		sf_error("stirling2", SF_ERROR_NO_RESULT,
 		         "integer overflow has occured");
-		return -1;
+		return 0;
 	      }
 	      tmp = curr[j - 1] * (i + 1);
 	      if (tmp > UINT64_MAX - curr[j]) {
 		free(curr);
 		sf_error("stirling2", SF_ERROR_NO_RESULT, 
 		         "integer overflow has occured");
-		return -1;
+		return 0;
 	      }
 	      curr[j] = tmp + curr[j];
           }
