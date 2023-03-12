34eb6d99ab1e21a419aa3b3d70baa179fd937894
steppi <albert_steppi@hms.harvard.edu>
2023-03-12 00:47:32 -0500

    Add back old docstring

--- scipy/special/_stirling2.py
+++ scipy/special/_stirling2.py
@@ -3,7 +3,45 @@ from scipy.special._ufuncs import _stirling2_approx
 
 
 def stirling2(n, k, exact=True):
-    """Needs updated docstring."""
+    """stirling2(n, k)
+
+    Stirling number of the second kind. Counts the number of non-empty subsets
+    of `n` set items that can be made with `k` non-empty subsets. Both `n` and
+    `k` should be non-negative integers, Negative integers for either `n` or
+    `k` cause the function to return `0`. If there is an error allocating the
+    array for the computation a `-1` is returned and if an overflow occurs
+    during the computation a `-2` is returned.
+
+    Parameters
+    ----------
+    n : array_like
+        The number of items in the set
+    k : array_like
+        The number of non-empty subsets to be made out of the n elements
+
+    Returns
+    -------
+    scalar or ndarray
+        The number of ways to make k non-empty subsets out of n elements
+
+    Examples
+    --------
+    >>> import scipy.special as sc
+    >>> sc.stirling2(0,0)
+    1
+    >>> sc.stirling2(3,0)
+    0
+    >>> sc.stirling2(4,2)
+    7
+    >>> sc.stirling2(4,3)
+    6
+    >>> sc.stirling2(26,10)
+    -2
+
+    See Also
+    --------
+    comb, factorial
+    """
     if exact:
         return _stirling2_pyint(n, k)
     else:
